02f65c388677f2a3e3a99832c448356de7921744
AlexMaxes <544046044@qq.com>
2019-12-27 14:55:31 +0800

    add admin list and filter and search

--- login/admin.py
+++ login/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
-admin.site.register(models.User)
-admin.site.register(models.ConfirmString)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'sex', 'c_time', 'has_confirmed']
+    search_fields = ['name', 'email', 'c_time']
+    list_filter = ['sex', 'has_confirmed', 'c_time']
+    ordering = ['-c_time']
+class ConfirmAdmin(admin.ModelAdmin):
+    list_display = ['user','c_time','code']
+    search_fields = ['user']
+    list_filter = ['c_time']
+    ordering = ['-c_time']
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.ConfirmString,ConfirmAdmin)
--- login/models.py
+++ login/models.py
@@ -9,12 +9,12 @@ class User(models.Model):
         ('female', "女"),
     )
 
-    name = models.CharField(max_length=128, unique=True)
-    password = models.CharField(max_length=256)
-    email = models.EmailField(unique=True)
-    sex = models.CharField(max_length=32, choices=gender, default="男")
-    c_time = models.DateTimeField(auto_now_add=True)
-    has_confirmed = models.BooleanField(default=False)
+    name = models.CharField('姓名', max_length=128, unique=True)
+    password = models.CharField('密码', max_length=256)
+    email = models.EmailField('邮箱', unique=True)
+    sex = models.CharField('性别', max_length=32, choices=gender, default="男")
+    c_time = models.DateTimeField('创建时间', auto_now_add=True)
+    has_confirmed = models.BooleanField('是否确认邮件', default=False)
 
     def __str__(self):
         return self.name
@@ -26,9 +26,9 @@ class User(models.Model):
 
 
 class ConfirmString(models.Model):
-    code = models.CharField(max_length=256)
+    code = models.CharField('确认码', max_length=256)
     user = models.OneToOneField('User', on_delete=models.CASCADE)
-    c_time = models.DateTimeField(auto_now_add=True)
+    c_time = models.DateTimeField('注册时间', auto_now_add=True)
 
     def __str__(self):
         return self.user.name + ":  " + self.code
